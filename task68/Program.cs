d4b5053f61e5ede366369177d96f76f5d010086f
Denis-Moiseev <mrtesla666@gmail.com>
2023-08-07 13:49:31 +0800

    finishTask68

---
+++ task68/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetSumNums(int num, int num2)
+{
+    if (num == 0) return num2 + 1;
+    if (num != 0 && num2 == 0) return GetSumNums(num - 1, 1);
+    if (num > 0 && num2 > 0) return GetSumNums(num - 1, GetSumNums(num, num2 - 1));
+    return GetSumNums(num, num2);
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInput("Введите число: ");
+int num2 = GetInput("Введите число: ");
+Console.Write($"m = {num}, n = {num2} -> A(m,n) = ");
+Console.Write(GetSumNums(num, num2));
