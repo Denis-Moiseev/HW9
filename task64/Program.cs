98fe72b3e2918d12c39330a8f21d292e95c71f81
Denis-Moiseev <mrtesla666@gmail.com>
2023-08-07 13:08:59 +0800

    finishTask64

---
+++ task64/Program.cs
@@ -0,0 +1,16 @@
+﻿int ShowIntegers(int n)
+{
+    if (n == 1) return 1;
+    Console.Write($"{n}, ");
+    return ShowIntegers(n - 1);
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInput("Введите число: ");
+Console.Write($"N = {num} -> ");
+Console.Write(ShowIntegers(num));
